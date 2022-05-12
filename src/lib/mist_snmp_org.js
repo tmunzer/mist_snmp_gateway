90b37abed046aaa1b663c430da24b4c6f3f826d2
Thomas Munzer <thomas.munzer@gmail.com>
2022-05-12 20:20:52 +0200

    adding Org Sle Percent

--- src/lib/mist_snmp_org.js
+++ src/lib/mist_snmp_org.js
@@ -3,12 +3,15 @@ const logger = require("./logger");
 
 
 function update_org(mib, org) {
-    if (!org.stats.sle.coverage) org.stats.sle.coverage = { total: -1, ok: -1 }
-    if (!org.stats.sle.failed_to_connect) org.stats.sle.failed_to_connect = { total: -1, ok: -1 }
-    if (!org.stats.sle.roaming) org.stats.sle.roaming = { total: -1, ok: -1 }
-    if (!org.stats.sle.time_to_connect) org.stats.sle.time_to_connect = { total: -1, ok: -1 }
-    if (!org.stats.sle.throughput) org.stats.sle.throughput = { total: -1, ok: -1 }
-    if (!org.stats.sle.capacity) org.stats.sle.capacity = { total: -1, ok: -1 }
+    const sles = ["coverage", "failed_to_connect", "roaming", "time_to_connect", "throughput", "capacity"]
+    var percent = {};
+    sles.forEach(sle => {
+        if (!org.stats.sle[sle]) {
+            org.stats.sle[sle] = { total: 0, ok: 0 }
+        }
+        if (org.stats.sle[sle].total > 0) percent[sle] = (org.stats.sle[sle].ok / org.stats.sle[sle].total) * 100
+        else percent[sle] = 0;
+    })
 
     mib.setScalarValue('orgName', org.name)
     mib.setScalarValue('orgId', org.id)
@@ -17,19 +20,30 @@ function update_org(mib, org) {
     mib.setScalarValue('numInventory', org.stats.num_inventory)
     mib.setScalarValue('numDevicesConnected', org.stats.num_devices_connected)
     mib.setScalarValue('numDevicesDisconnected', org.stats.num_devices_disconnected)
+
     mib.setScalarValue('sleCoverageTotal', org.stats.sle.coverage.total)
     mib.setScalarValue('sleCoverageOk', org.stats.sle.coverage.ok)
+    mib.setScalarValue('sleCoveragePercent', percent.coverage)
+
     mib.setScalarValue('sleFailedToConnectTotal', org.stats.sle.failed_to_connect.total)
     mib.setScalarValue('sleFailedToConnectOk', org.stats.sle.failed_to_connect.ok)
+    mib.setScalarValue('sleFailedToConnectPercent', percent.failed_to_connect)
+
     mib.setScalarValue('sleRoamingTotal', org.stats.sle.roaming.total)
     mib.setScalarValue('sleRoamingOk', org.stats.sle.roaming.ok)
+    mib.setScalarValue('sleRoamingPercent', percent.roaming)
+
     mib.setScalarValue('sleTimeToConnectTotal', org.stats.sle.time_to_connect.total)
     mib.setScalarValue('sleTimeToConnectOk', org.stats.sle.time_to_connect.ok)
+    mib.setScalarValue('sleTimeToConnectPercent', percent.time_to_connect)
+
     mib.setScalarValue('sleThroughputTotal', org.stats.sle.throughput.total)
     mib.setScalarValue('sleThroughputOk', org.stats.sle.throughput.ok)
+    mib.setScalarValue('sleThroughputPercent', percent.throughput)
+
     mib.setScalarValue('sleCapacityTotal', org.stats.sle.capacity.total)
     mib.setScalarValue('sleCapacityOk', org.stats.sle.capacity.ok)
-    mib.getScalarValue('orgName')
+    mib.setScalarValue('sleCapacityPercent', percent.capacity)
 
 }
 

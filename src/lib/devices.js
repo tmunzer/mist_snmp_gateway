ec24db651645c96c89227897121cacd147754a27
Thomas Munzer <tmunzer@juniper.net>
2023-07-17 10:51:54 +0200

    set request limit=1000 and type=all

--- src/lib/devices.js
+++ src/lib/devices.js
@@ -1,10 +1,10 @@
 const req = require("./req");
 
-module.exports.stats = function(host, token, site_id, device_type = "ap", cb) {
-    url = "https://" + host + "/api/v1/sites/" + site_id + "/stats/devices?type=" + device_type;
+module.exports.stats = function(host, token, site_id, device_type = "all", cb) {
+    url = "https://" + host + "/api/v1/sites/" + site_id + "/stats/devices?type=" + device_type + "&limit=1000";
     resp = req.GET({ host: host, token: token }, url, (err, data) => cb(err, data))
 }
-module.exports.list = function(host, token, site_id, device_type = "ap", cb) {
-    url = "https://" + host + "/api/v1/sites/" + site_id + "/devices?type=" + device_type;
+module.exports.list = function(host, token, site_id, device_type = "all", cb) {
+    url = "https://" + host + "/api/v1/sites/" + site_id + "/devices?type=" + device_type + "&limit=1000";
     resp = req.GET({ host: host, token: token }, url, (err, data) => cb(err, data))
 }
